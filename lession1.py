821a10f4d92afdaf2cc8cc612810f89a69531f38
nikolozkurtskhalidze <143752995+nikolozkurtskhalidze@users.noreply.github.com>
2023-09-01 00:06:55 +0400

    Add files via upload

---
+++ lession1.py
@@ -0,0 +1,147 @@
+from turtle import *
+
+speed(6)
+width(7)
+color("yellow")
+begin_fill()
+forward(200)
+left(90)
+
+forward(200)
+left(90)
+
+forward(200)
+left(90)
+
+forward(200)  #end of square
+end_fill()
+left(90)
+
+forward(90)    #start of dor
+end_fill()
+color("black")
+begin_fill()
+left(90)
+forward(90)   #up
+right(90)
+forward(60)   #left
+right(90)
+forward(90)      #down
+right(180)
+forward(90)
+end_fill()
+color("white")
+forward(30)
+color("black")
+left(90)
+forward(50)
+right(90)
+forward(40)
+right(90)
+forward(50)
+right(90)
+forward(40)
+right(90)
+forward(25)
+right(90)
+forward(40)
+right(180)
+forward(20)
+right(90)
+forward(20)
+left(180)
+forward(40)
+
+penup()
+goto(200, 200)
+pendown()
+color ("blue")
+begin_fill()
+left(120)
+forward(200)
+left(120)
+forward(200)
+left(120)
+forward(200)
+end_fill()
+color("yellow")
+right(90)
+forward(200)
+right(90)
+forward(50)
+color("black")
+begin_fill()
+forward(60)
+right(90)
+forward(90)
+right(90)
+forward(60)
+right(90)
+forward(90)
+end_fill()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+exitonclick()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#print("nikoloz kurtskhalidze")
+
+#comment for humans 
+
+#a number like 9 we call it int=integer 
+#when something is inside this like a word or a letter "" it will be called #string+str
+
+
+
+#print(5 +int(5)) 
+
+#print("niko aris " + "5 " + "wlis")
